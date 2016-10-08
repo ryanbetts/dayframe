90e42a836181cd1cd9d23315b61e69eed05c70a5
Ryan Betts <ryan@ishuman.co>
2016-10-07 19:38:01 -0700

    added port env variable for easier deployes to services like Heroku

--- index.js
+++ index.js
@@ -106,6 +106,7 @@ io.on('connection', function(socket){
     });
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+var port = process.env.PORT || 3000
+http.listen(port, function(){
+    console.log('listening on *:'+port);
 });
